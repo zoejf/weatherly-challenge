795cb43f7efe922987a7955c3eed3297a4089194
zoejf <zoejfoss@gmail.com>
2015-10-01 17:45:46 -0700

    gets api data

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 angular.module('weatherly', [])
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    
+
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.city = [];
+    $scope.searchCity = function  () {
+      console.log($scope.city);
+      var url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.city +'&type=accurate&cnt=4&units=imperial';
+      $http.get(url)
+        .then(function (response) {
+          console.log(response.data);
+          $scope.city = response.data;
+        });
+    };
   }]);
