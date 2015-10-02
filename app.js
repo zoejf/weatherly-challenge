af1a04ccf56874c25105073c6bf033623dab69c2
zoejf <zoejfoss@gmail.com>
2015-10-01 20:54:01 -0700

    weather images show for diff codes

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ angular.module('weatherly', [])
         .then(function (response) {
           $scope.city = response.data;
           $scope.showIcon = function (index) {
-            console.log(response.data.list[index].weather[0].id);
+            if (response.data.list[index].weather[0].id <= 299) {
+              return 'thunderstorm'
+            } else if (response.data.list[index].weather[0].id <= 399) {
+              return 'drizzle'
+            } else if (response.data.list[index].weather[0].id <= 599) {
+              return 'rain'
+            } else if (response.data.list[index].weather[0].id <= 699) {
+              return 'snow'
+            } else if (response.data.list[index].weather[0].id <= 799) {
+              return 'atmosphere'
+            } else if (response.data.list[index].weather[0].id <= 899) {
+              return 'clouds'
+            } else if (response.data.list[index].weather[0].id <= 949) {
+              return 'extreme'
+            } else if (response.data.list[index].weather[0].id <= 999) {
+              return 'additional'
+            };
           };
         });
     };
